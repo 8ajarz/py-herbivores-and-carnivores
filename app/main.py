8e3a64d2909ffc4474594118ceb3127bcf44b8ea
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-06-05 23:29:57 +0700

    there is only one issue with __repr__ method and how test_print_animal_alive accepts it

--- app/main.py
+++ app/main.py
@@ -1,51 +1,21 @@
 from __future__ import annotations
 
 
-class Animal_desc():
-
-    def __init__(self) -> None:
-        self.creatures = []
-
-    def __set__(self, instance, value):
-        self.creatures = value
-
-    def __get__(self, instance, owner):
-        return self
-
-    def add_creature(self, obj) -> None:
-        self.creatures.append(obj)
-
-    def __str__(self):
-        return str([
-            {
-                "Name": creature.name,
-                "Health": creature.health,
-                "Hidden": creature.hidden
-            }
-            for creature in self.creatures
-        ])
-
-    def __len__(self) -> int:
-        return len(self.creatures)
-
-    def __getitem__(self, index):
-        return self.creatures[index]
-
-    def __iter__(self):
-        return iter(self.creatures)
-
-    def remove_creature(self, obj) -> None:
-        self.creatures.remove(obj)
-
-
 class Animal:
-    alive = Animal_desc()
+    alive = []
 
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, health: int = 100) -> None:
         self.name = name
-        self.health = 100
+        self.health = health
         self.hidden = False
-        Animal.__dict__['alive'].add_creature(self)
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
 
 
 class Herbivore(Animal):
@@ -58,9 +28,7 @@ class Carnivore(Animal):
         if isinstance(other, Herbivore) and not other.hidden:
             other.health -= 50
             if other.health <= 0:
-                for indx, creature in enumerate(Animal.alive):
-                    if creature is other:
-                        Animal.alive.remove_creature(creature)
+                Animal.alive.remove(other)
 
 
 if __name__ == "__main__":
